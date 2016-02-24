fa6fa733c382a2fd38e238381f341c9d6d91e11a
loldi <andrew@Andrews-MBP-2.home>
2016-02-24 18:07:41 -0500

    Changed to fit the annoying ball "game"

---
+++ Homework Week 4/Week4Homework/Week4Homework.ino
@@ -0,0 +1,70 @@
+
+
+// These constants won't change:
+const int sensorPin = A0;
+const int potPin = A1;   // pin that the sensor is attached to
+const int ledPin = 9; 
+
+int buzzer = 3;
+
+// variables:
+int sensorValue = 0;         // the sensor value
+int sensorMin = 1023;        // minimum sensor value
+int sensorMax = 0;           // maximum sensor value
+
+int potValue = 0;
+int potMax = 0;
+int potMin = 1023;
+
+
+void setup() {
+  // turn on LED to signal the start of the calibration period:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  // calibrate during the first five seconds
+  while (millis() < 5000) {
+    sensorValue = analogRead(sensorPin);
+
+    // record the maximum sensor value
+    if (sensorValue > sensorMax) {
+      sensorMax = sensorValue;
+    }
+
+    // record the minimum sensor value
+    if (sensorValue < sensorMin) {
+      sensorMin = sensorValue;
+    }
+  }
+
+  // signal the end of the calibration period
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+  // read the sensor:
+  sensorValue = analogRead(sensorPin);
+
+  potValue = analogRead(potPin);
+
+  potValue = map(potValue, potMin, potMax, 0, 4400);
+
+  // apply the calibration to the sensor reading
+  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 255);
+
+  // in case the sensor value is outside the range seen during calibration
+  sensorValue = constrain(sensorValue, 0, 255);
+
+  // fade the LED using the calibrated value:
+  analogWrite(ledPin, sensorValue);
+  Serial.println(sensorValue);
+
+  if (sensorValue < 230){
+    tone(buzzer, potValue);
+    delay(100);
+    noTone(buzzer);
+  } else {
+    noTone(buzzer);
+  }
+}
